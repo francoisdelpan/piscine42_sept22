03cd9f307967d1c450ed55e4222263797626eb7c
Francois Pannier <fpannier@e3r7p1.clusters.42paris.fr>
2022-09-15 11:00:41 +0200

    ex05

--- C_Piscine_C_00/ex05/ft_print_comb.c
+++ C_Piscine_C_00/ex05/ft_print_comb.c
@@ -1,30 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpannier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/15 09:04:37 by fpannier          #+#    #+#             */
+/*   Updated: 2022/09/15 11:00:14 by fpannier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void	 ft_print_comb(void)
+void	super_write(char d1, char d2, char d3)
 {
-	char c1 = 48;
-	char c2 = 48;
-	char c3 = 48;
-
-	while( c1 <= 55 )
+	write(1, &d1, 1);
+	write(1, &d2, 1);
+	write(1, &d3, 1);
+	if (!(d1 == '7' && d2 == '8' && d3 == '9'))
 	{
-		while( c2 <= 57 )
-		{
-			while( c3 <= 57 )
-			{
-				if( c1 != c2 && c1 != c3 && c2 != c3 )
-				{
-					write( 1, &c1&c2&c3",", 4 );
-				}
-			}
-		}
+		write(1, ",", 1);
 	}
-
 }
 
-int 	main(void)
+void	ft_print_comb(void)
+{
+	char	c1;
+	char	c2;
+	char	c3;
+
+	c1 = '0';
+	c2 = '0';
+	c3 = '0';
+	while (c1 <= '7')
+	{
+		while (c2 <= '8')
+		{
+			while (c3 <= '9')
+			{
+				if (c1 != c2 && c1 != c3 && c2 != c3)
+				{
+					super_write(c1, c2, c3);
+				}
+				c3++;
+			}
+			c3 = '0';
+			c2++;
+		}
+		c2 = '0';
+		c1++;
+	}
+}
+
+int	main(void)
 {
 	ft_print_comb();
-
 	return (0);
 }
