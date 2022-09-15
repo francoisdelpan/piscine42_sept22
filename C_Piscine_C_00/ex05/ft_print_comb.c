2e6e2647e4417eb4b38cff247a1d07b832bb481f
Francois Pannier <fpannier@e3r7p1.clusters.42paris.fr>
2022-09-15 09:01:06 +0200

    -b

--- C_Piscine_C_00/ex05/ft_print_comb.c
+++ C_Piscine_C_00/ex05/ft_print_comb.c
@@ -2,7 +2,23 @@
 
 void	 ft_print_comb(void)
 {
+	char c1 = 48;
+	char c2 = 48;
+	char c3 = 48;
 
+	while( c1 <= 55 )
+	{
+		while( c2 <= 57 )
+		{
+			while( c3 <= 57 )
+			{
+				if( c1 != c2 && c1 != c3 && c2 != c3 )
+				{
+					write( 1, &c1&c2&c3",", 4 );
+				}
+			}
+		}
+	}
 
 }
 
