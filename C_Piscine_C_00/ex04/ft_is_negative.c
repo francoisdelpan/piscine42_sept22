5f273026fe477c5b76177395ba85be15f03162c1
Francois Pannier <fpannier@e3r7p1.clusters.42paris.fr>
2022-09-15 08:11:02 +0200

    ex03 et ex04

---
+++ C_Piscine_C_00/ex04/ft_is_negative.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	 ft_is_negative(int n)
+{
+	if( n >= 0 )
+	{
+		write( 1, "P\n", 2 );
+	} else
+	{
+		write( 1, "N\n", 2 );
+	}	
+}
+
+int	main(void)
+{
+	ft_is_negative( 1 );
+	
+	return (0);
+}
+
