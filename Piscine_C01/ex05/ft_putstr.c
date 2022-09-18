3e80eae7e4960f17c6b5b1af4e63f772ca9210e9
Francois Pannier <fpannier@e3r7p1.clusters.42paris.fr>
2022-09-18 23:36:17 +0200

    ex05

--- Piscine_C01/ex05/ft_putstr.c
+++ Piscine_C01/ex05/ft_putstr.c
@@ -6,29 +6,32 @@
 /*   By: fpannier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 15:44:09 by fpannier          #+#    #+#             */
-/*   Updated: 2022/09/17 16:08:10 by fpannier         ###   ########.fr       */
+/*   Updated: 2022/09/18 23:35:47 by fpannier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char *str)
+void	ft_putstr(char *str)
 {
 	int	index;
 	char	result;
 
 	index = 0;
-	result = *str[index];
-	while (result != "\0")
+	result = str[index];
+	while (result != '\0')
 	{
-		
+		write(1, &result, 1);
+		index++;
+		result = str[index];
 	}
 }
 
 int	main(void)
+
 {
 	char	*hello;
 
-	*hello = "Hello World!";
-	ft_putstr(*hello);
+	hello = "Hello World!";
+	ft_putstr(hello);
 }
