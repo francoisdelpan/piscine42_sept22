90301bfa712676c181717aa2502a5947ccc6b1bb
Francois Pannier <fpannier@e3r7p1.clusters.42paris.fr>
2022-09-19 09:31:37 +0200

    ex07

--- Piscine_C01/ex07/ft_rev_int_tab.c
+++ Piscine_C01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: fpannier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 23:52:34 by fpannier          #+#    #+#             */
-/*   Updated: 2022/09/19 00:16:05 by fpannier         ###   ########.fr       */
+/*   Updated: 2022/09/19 09:31:11 by fpannier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,33 @@ void	ft_rev_int_tab(int *tab, int size)
 {
 	int	temp;
 	int	index;
+	int	temp_size;
 
 	index = 0;
-	size--;
-	while (index <= (size / 2))
+	temp_size = size - 1;
+	while (index < (size / 2))
 	{
 		temp = tab[index];
-		tab[index] = tab[size];
-		tab[size] = temp;
+		tab[index] = tab[temp_size];
+		tab[temp_size] = temp;
 		index++;
-		size--;
+		temp_size--;
 	}
 }
 
 int	main(void)
 {
-	int	*tab_test;
+	int	tab_test[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int	size_test;
 	int	i;
 
-	size_test = 6;
-	tab_test[0] = 1;
-	tab_test[1] = 2;
-	tab_test[2] = 3;
-	tab_test[3] = 4;
-	tab_test[4] = 5;
-	tab_test[5] = 6;
+	size_test = 9;
 	ft_rev_int_tab(tab_test, size_test);
 	i = 0;
 	while (i < size_test)
 	{
-		tab_test[i];
+		printf("%d", tab_test[i]);
+		i++;
 	}
 	return (0);
 }
