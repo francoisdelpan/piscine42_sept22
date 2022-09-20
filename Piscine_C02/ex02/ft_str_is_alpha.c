020cf5fa888641bf0017f81e76addf5a34c1adf1
Francois Pannier <fpannier@e3r7p1.clusters.42paris.fr>
2022-09-20 09:41:30 +0200

    ex02

---
+++ Piscine_C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpannier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 09:20:35 by fpannier          #+#    #+#             */
+/*   Updated: 2022/09/20 09:40:48 by fpannier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	while (*str != '\0')
+	{
+		if ((*str < 65 || *str > 90) && (*str < 97 || *str > 122))
+		{
+			printf("%c\n", *str);
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_str_is_alpha("Hello654 World");
+	printf("%d\n", result);
+}
