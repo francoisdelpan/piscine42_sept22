a7105def6f41f876e3a600b0956a340fedadcc19
Francois Pannier <fpannier@e3r7p1.clusters.42paris.fr>
2022-09-21 19:54:22 +0200

    ex02

---
+++ Piscine_C03/ex02/ft_strcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpannier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 17:56:03 by fpannier          #+#    #+#             */
+/*   Updated: 2022/09/21 19:53:59 by fpannier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	size_dest;
+
+	size_dest = 0;
+	while (dest[size_dest] != '\0')
+	{
+		size_dest++;
+	}
+	printf("size_dest: %d | *src: %c\n", size_dest, *src);
+	while (*src != '\0')
+	{
+		printf("size_dest: %d | *src: %c | dest: %s\n", size_dest, *src, dest);
+		dest[size_dest] = *src;
+		src++;
+		size_dest++;
+	}
+	dest[size_dest] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char	result[3] = "ABA";
+
+	ft_strcat(result, "ABBSG");
+	printf("result : %s\n", result);
+	return (0);
+}
