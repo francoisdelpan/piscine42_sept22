0ea6e9a28e91176d5e9138560e9c0a7d6df82e16
Francois Pannier <fpannier@e3r7p1.clusters.42paris.fr>
2022-09-24 19:38:47 +0200

    parsing

--- Piscine_Rush01/parsing.c
+++ Piscine_Rush01/parsing.c
@@ -6,7 +6,7 @@
 /*   By: fpannier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 12:38:09 by fpannier          #+#    #+#             */
-/*   Updated: 2022/09/24 16:06:54 by fpannier         ###   ########.fr       */
+/*   Updated: 2022/09/24 19:38:35 by fpannier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,21 @@ void    int_tab_print(int *to_print)
 	int     i;
 
 	i = 0;
-	while (to_print[i] != '\0')
+	while (to_print[i])
 	{
 		printf("%d, ", to_print[i]);
 		i++;
 	}
 	printf("\n");
 }
+
+/**
+ * Count and return length of a char list
+ *
+ * @parent : check_str()
+ * @param : any char list
+ * @return : length of the char list
+ */
 
 int	get_tab_length(char *str)
 {
@@ -46,43 +54,92 @@ int	get_tab_length(char *str)
 	return (i);
 }
 
-int	check_length(int *clues_to_test, int board_length)
-{
-	int	clues_length;
+/**
+ * Check the munber of 1, 2, 3, 4
+ * Exemple : more than four 4 is forbidden
+ *
+ * @parent : check_length()
+ * @params : list of digit count
+ * @return : 1 if the conditions are respected
+ *	     0 otherwise
+ * -> Go back to the main
+ */
 
-	clues_length = sizeof(clues_to_test) / sizeof(int *);
-	printf("clues size: %d\n", clues_length);
-	if (clues_length == (board_length * 4))
+int	check_digit(int *digits)
+{
+	if (
+		digits[3] <= 4 && digits[2] <= 8
+		&& digits[1] <= 12 && digits[0] <= 12
+		&& (digits[0] + digits[1] + digits[2]
+		+ digits[3] == 16)
+	)
 	{
 		return (1);
 	}
 	return (0);
 }
 
-int	check_str(char *str, int *board_length, int *clues_to_test)
+/**
+ * Check the clues length, count of many 1, 2, 3 & 4 there is
+ * 
+ * @parent : check_str()
+ * @params : clues 
+ * @return : 0 if the size != 16
+ *	     check_digit() for further investigation 
+ */
+
+int	check_length(int *clues_to_test)
+{
+	int	clues_length;
+	int	digit_nbr[4] = {0, 0, 0, 0};
+
+	clues_length = 0;
+	while (clues_to_test[clues_length])
+	{
+		digit_nbr[clues_to_test[clues_length] - 1] += 1;
+		clues_length++;
+	}
+	// printf("clues size: %d\n", clues_length);
+	// int_tab_print(digit_nbr);
+	if (clues_length == 16)
+	{
+		return check_digit(digit_nbr);
+	}
+	return (0);
+}
+
+/**
+ * Check the string comming from argv[1]
+ * 
+ * @parent : parsing()
+ * @params : argv[1], clues to fill
+ * @return : 0 if false
+ *	     check_length() for further investigation
+ */
+
+int	check_str(char *str, int *clues_to_test)
 {
 	int	str_length;
 	int	i;
 
 	str_length = get_tab_length(str);
+	printf("str_length : %d\n", str_length);
 	i = 0;
-	if ((str_length + 1) % 8 == 0
-		&& str_length >= 16
-		&& str_length <= 72)
+	if ( str_length == 31)
 	{
-		*board_length = (str_length + 1) / 8;
-		printf("Length board : %d\n", *board_length);
 		while (i < str_length)
 		{
 			if (i % 2 == 0
 				&& str[i] >= '1'
-				&& str[i] <= *board_length)
+				&& str[i] <= '4')
 			{
 				clues_to_test[i / 2] = str[i] - '0';
+				printf("str[i] : %d | -0 : %d\n", str[i], str[i] - 48);
+				int_tab_print(clues_to_test);
 			}
 			i++;
 		}
-		return (check_length(clues_to_test, *board_length));
+		return (check_length(clues_to_test));
 	}
 	return (0);
 }
@@ -90,36 +147,35 @@ int	check_str(char *str, int *board_length, int *clues_to_test)
 /**
  * Main function of parsing argv
  *
- * @params : argc, argv directly from main
+ * @parent : main()
+ * @params : argc, argv directly from main, pointer of clues
  * @return : 0 if the string is unavailable
  *	     1 if the string is available
  */
 
-int	*parsing(int argc, char *argv[])
+int	parsing(int argc, char *argv[], int *p_clues)
 {
-	int	board_length;
-	int	*p_board_length;
-	int	clues[72];
 	int	result;
 
-	board_length = 0;
-	p_board_length = &board_length;
 	if (argc == 2)
 	{
-		result = check_str(argv[1],  p_board_length, clues);
-		if (result == 0)
+		result = check_str(argv[1], p_clues);
+		if (result == 1)
 		{
-			write(2, "HereError\n", 10);
+			return (1);
 		}
-		return (clues);
 	}
-	write(2, "ThereError\n", 11);
+	write(2, "Error\n", 6);
 	return (0);
 }
 
 int	main(int argc, char *argv[])
 {
-	char	result[] = parsing(argc, argv);
-	int_tab_print(result);
+	int	clues[17] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int	result;
+
+	result = parsing(argc, argv, clues);
+	printf("result: %d\n", result);
+	int_tab_print(clues);
 	return (0);
 }
